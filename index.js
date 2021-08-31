da748222ba6ae501c2239a160d5b2fb78b38c7f0
Hammad <mhnaeem@mun.ca>
2021-08-31 01:21:07 -0230

    Exposing the absolute file paths to the webpack loaders inside the index.js file

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 /**
  * @fileoverview Public facing file
  */
+const path = require('path');
 
 const SoyBuilder = require("./src/builder");
 const SoyCompileLoader = require("./webpack/soy_compile");
 const SoyPostProcessorLoader = require("./webpack/soy_postprocessor");
 const SoyPreProcessorLoader = require("./webpack/soy_preprocessor");
 
+const Webpack = {
+    SoyCompileLoader: path.join(__dirname, "./webpack/soy_compile"),
+    SoyPostProcessorLoader: path.join(__dirname, "./webpack/soy_postprocessor"),
+    SoyPreProcessorLoader: path.join(__dirname, "./webpack/soy_preprocessor")
+}
+
 module.exports = {
+    Webpack,
     SoyBuilder,
     SoyCompileLoader,
     SoyPostProcessorLoader,
